709e4c62c08b3823c3c520d4d7b683c524db4115
Kinzume <81969626+Kinzume@users.noreply.github.com>
2022-11-16 14:24:02 +0000

    Install Orbitron Font Family

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import localFont from "@next/font/local";
+
+const orbitron = localFont({
+  src: [
+    { path: "./Orbitron-Regular.ttf", weight: "400" },
+    { path: "./Orbitron-Medium.ttf", weight: "500" },
+    { path: "./Orbitron-Bold.ttf", weight: "700" },
+    { path: "./Orbitron-Black.ttf", weight: "900" },
+  ],
+  variable: "--orbitron",
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${orbitron.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
