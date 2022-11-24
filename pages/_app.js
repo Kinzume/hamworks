51f469906409caf28c2b6faee482d4bf99680af8
Kinzume <81969626+Kinzume@users.noreply.github.com>
2022-11-24 14:44:08 +0000

    Add checkered bg to body

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ const orbitron = localFont({
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className={`${orbitron.variable} font-sans`}>
+    <div className={`${orbitron.variable} font-sans body-bg`}>
       <Component {...pageProps} />
     </div>
   );
