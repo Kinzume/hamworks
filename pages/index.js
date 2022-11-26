bd0e8b5dc1d435214a53c3d6a1688746df090b89
Kinzume <81969626+Kinzume@users.noreply.github.com>
2022-11-26 13:35:39 +0000

    Add responsive about section

--- pages/index.js
+++ pages/index.js
@@ -112,7 +112,7 @@ export default function Home() {
             />
           ))}
         </section>
-        <section className="flex flex-col items-center space-y-6 mt-20">
+        <section className="flex flex-col items-center space-y-6 mt-20 text-center">
           <div className="overflow-hidden rounded-full shadow-avatar">
             <Image
               className="scale-105"
@@ -122,31 +122,36 @@ export default function Home() {
               height={163}
             />
           </div>
-          <h2 className="sr-only">About: James Currie and Hamworks</h2>
-          <span className="text-lg">James Currie | Hamworks</span>
-          <p className="text-center mx-auto max-w-xs">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-            beatae dolore rem officiis maxime vel perspiciatis. Maiores ipsa
-            nobis in voluptatum dolorum cumque debitis odio? Laboriosam, ducimus
-            fugiat doloribus ipsa quo totam impedit. Enim, in?
-          </p>
-          <div className="flex space-x-4">
-            <a
-              href=""
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="sr-only">Facebook Profile</span>
-              <FaFacebookF className="text-[#00b7ff] text-3xl" />
-            </a>
-            <a
-              href=""
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="sr-only">Twitter Profile</span>
-              <FaTwitter className="text-[#00b7ff] text-3xl" />
-            </a>
+          <div className="container space-y-5 sm:space-y-10">
+            <h2 className="sr-only">About: James Currie and Hamworks</h2>
+            <p className="text-xl sm:text-2xl md:text-3xl">
+              James Currie | Hamworks
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Blanditiis beatae dolore rem officiis maxime vel perspiciatis.
+              Maiores ipsa nobis in voluptatum dolorum cumque debitis odio?
+              Laboriosam, ducimus fugiat doloribus ipsa quo totam impedit. Enim,
+              in?
+            </p>
+            <div className="flex space-x-4 justify-center">
+              <a
+                href=""
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="sr-only">Facebook Profile</span>
+                <FaFacebookF className="text-[#00b7ff] text-3xl" />
+              </a>
+              <a
+                href=""
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="sr-only">Twitter Profile</span>
+                <FaTwitter className="text-[#00b7ff] text-3xl" />
+              </a>
+            </div>
           </div>
         </section>
         <section className="bg-[#D6D6D6] text-black py-4 px-2 mt-16">
